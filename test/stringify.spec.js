556df36540602020bd81fae60a2d0adf0b2861ac
Marco Baumgartl <marco.baumgartl@stock3.com>
2025-12-03 14:06:14 +0100

    test: Use Node.js test runner & assertions

--- test/stringify.spec.js
+++ test/stringify.spec.js
@@ -1,85 +1,68 @@
 'use strict';
 
-const { expect } = require('chai');
+const { describe, it } = require( 'node:test');
+const assert = require( 'node:assert/strict');
 const stringify = require('../src/util/stringify');
 
 describe('stringify select', () => {
-    context('tests copied from github.com/godmodelabs/flora-client-js', () => {
-        it('should stringify simple arrays', () => {
-            expect(stringify(['id', 'name'])).to.equal('id,name');
-        });
-
-        it('should handle objects', () => {
-            expect(stringify({ groupA: ['attr1', 'attr2'] })).to.equal('groupA[attr1,attr2]');
-        });
-
-        it('should handle simple key/value objects', () => {
-            expect(stringify({ key: 'value' })).to.equal('key.value');
-        });
-
-        it('should handle nested arrays', () => {
-            const spec = [
-                'id',
-                'name',
-                ['attr1', 'attr2'],
-            ];
-
-            expect(stringify(spec)).to.equal('id,name,attr1,attr2');
-        });
-
-        it('should handle nested objects', () => {
-            const spec = {
-                group1: { group2: 'value' },
-            };
-
-            expect(stringify(spec)).to.equal('group1.group2.value');
-        });
-
-        it('should handle nested items', () => {
-            const spec = [
-                'id',
-                'name',
-                { subGroup: ['attr1', 'attr2'] },
-                'attr',
-            ];
-
-            expect(stringify(spec)).to.equal('id,name,subGroup[attr1,attr2],attr');
-        });
-
-        it('should handle deeply nested items', () => {
-            const spec = [
-                'id',
-                'name',
-                {
-                    subGroupA: [
-                        'id',
-                        'name',
-                        {
-                            subSubGroupA: ['attr1', 'attr2'],
-                            subSubGroupB: [
-                                { subSubSubGroupA: ['attr1', 'attr2'] },
-                                'subSubSubItem',
-                                { subSubSubGroupB: ['attr1', 'attr2'] },
-                            ],
-                        },
-                    ],
-                },
-                'attr',
-            ];
-
-            const selectString = 'id,name,subGroupA[id,name,subSubGroupA[attr1,attr2],subSubGroupB[subSubSubGroupA[attr1,attr2],subSubSubItem,subSubSubGroupB[attr1,attr2]]],attr';
-            expect(stringify(spec)).to.equal(selectString);
-        });
-
-        it('should not use brackets for single item groups', () => {
-            expect(stringify({ subGroup: ['attr'] })).to.equal('subGroup.attr');
-        });
+    it('should stringify simple arrays', () => {
+        assert.equal(stringify(['id', 'name']), 'id,name');
     });
 
-    context('found bugs', () => {
+    it('should handle objects', () => {
+        assert.equal(stringify({ groupA: ['attr1', 'attr2'] }), 'groupA[attr1,attr2]');
+    });
+
+    it('should handle simple key/value objects', () => {
+        assert.equal(stringify({ key: 'value' }), 'key.value');
+    });
+
+    it('should handle nested arrays', () => {
+        const spec = ['id', 'name', ['attr1', 'attr2']];
+        assert.equal(stringify(spec), 'id,name,attr1,attr2');
+    });
+
+    it('should handle nested objects', () => {
+        const spec = { group1: { group2: 'value' } };
+        assert.equal(stringify(spec), 'group1.group2.value');
+    });
+
+    it('should handle nested items', () => {
+        const spec = ['id', 'name', { subGroup: ['attr1', 'attr2'] }, 'attr'];
+        assert.equal(stringify(spec), 'id,name,subGroup[attr1,attr2],attr');
+    });
+
+    it('should handle deeply nested items', () => {
+        const spec = [
+            'id',
+            'name',
+            {
+                subGroupA: [
+                    'id',
+                    'name',
+                    {
+                        subSubGroupA: ['attr1', 'attr2'],
+                        subSubGroupB: [{ subSubSubGroupA: ['attr1', 'attr2'] }, 'subSubSubItem', { subSubSubGroupB: ['attr1', 'attr2'] }],
+                    },
+                ],
+            },
+            'attr',
+        ];
+
+        assert.equal(
+            stringify(spec),
+            'id,name,subGroupA[id,name,subSubGroupA[attr1,attr2],subSubGroupB[subSubSubGroupA[attr1,attr2],subSubSubItem,subSubSubGroupB[attr1,attr2]]],attr',
+        );
+    });
+
+    it('should not use brackets for single item groups', () => {
+        assert.equal(stringify({ subGroup: ['attr'] }), 'subGroup.attr');
+    });
+
+    describe('found bugs', () => {
         it('deep object are not correctly parsed', () => {
-            const toParse = [{ a: [{ b: ['bb'], c: ['cc'] }] }];
-            expect(stringify(toParse)).to.be.equal('a[b.bb,c.cc]');
+            const spec = [{ a: [{ b: ['bb'], c: ['cc'] }] }];
+            assert.equal(stringify(spec), 'a[b.bb,c.cc]');
         });
     });
 });
